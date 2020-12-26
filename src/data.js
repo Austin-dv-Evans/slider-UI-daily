0cbf8478586f6eed2746c80d5dd2c22ecf247010
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-12-26 15:23:28 -0700

    slider active class complete

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const people = [
+  {
+    id: 1,
+    image:
+      "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/122458182_10157392099511283_5102256130180240907_n.jpg?_nc_cat=103&ccb=2&_nc_sid=09cbfe&_nc_ohc=5GfYiR2jM_UAX8Idm5P&_nc_ht=scontent.fapa1-1.fna&oh=f225bdb240fe6a34c9657af3ef0273b7&oe=6006E609",
+    name: "maria ferguson",
+    title: "office manager",
+    quote:
+      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
+  },
+  {
+    id: 2,
+    image:
+      "https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/121619176_2041913392609731_5869671214843247918_o.jpg?_nc_cat=100&ccb=2&_nc_sid=09cbfe&_nc_ohc=jF4eoZxYneMAX_37vPF&_nc_ht=scontent.fapa1-2.fna&oh=de5f72edf089645b2792321671a56f5d&oe=60074263",
+    name: "john doe",
+    title: "regular guy",
+    quote: "lorem ipsum lasdkjfa;sdjf;adsjfa;dskjf;ad",
+  },
+  {
+    id: 3,
+    image:
+      "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/131307917_2101884839945919_7694038603738872618_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_ohc=iIwLhKziN8oAX-H5MHD&_nc_ht=scontent.fapa1-1.fna&oh=adc9dfa426aadcf18b3552476c1cdee9&oe=6005C223",
+    name: "peter smith",
+    title: "product designer",
+    quote:
+      "Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.",
+  },
+  {
+    id: 4,
+    image:
+      "https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/121619176_2041913392609731_5869671214843247918_o.jpg?_nc_cat=100&ccb=2&_nc_sid=09cbfe&_nc_ohc=jF4eoZxYneMAX_37vPF&_nc_ht=scontent.fapa1-2.fna&oh=de5f72edf089645b2792321671a56f5d&oe=60074263",
+    name: "susan andersen",
+    title: "the boss",
+    quote:
+      "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ",
+  },
+];
+
+export default people;
